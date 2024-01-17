6a72a7260fda6199811317ec818afe8c99299ae6
Sergey Martirosyan <martirosyanseryoga@gmail.com>
2024-01-17 20:16:32 +0400

    Update script.js

--- Front-end2/script.js
+++ Front-end2/script.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', fetchData);
 
 function fetchData() {
-  // Access the data from local storage
   const token = localStorage.getItem('token');
-  // Replace 'YOUR_SERVER_URL' with your actual server URL
   fetch('http://localhost:8080/api/v1/lighter', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
-      // Add any additional headers if required
     },
   })
   .then(response => response.json())
@@ -18,10 +15,8 @@ function fetchData() {
 }
 
 function displayData(data) {
-  // Access the keys 'name' and 'price' from the data and create table rows
   const tableBody = document.querySelector('#data-table tbody');
 
-  // Clear existing rows
   tableBody.innerHTML = '';
 
   data.forEach(item => {
@@ -33,33 +28,27 @@ function displayData(data) {
     cellName.textContent = item.name;
     cellPrice.textContent = item.price;
 
-    // Create a button for deletion
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.addEventListener('click', () => {
         deleteItem(item.id);
-    }); // Assuming there is an 'id' property in your data
+    }); 
 
-    // Append the button to the cell
 
-    // Create a button for updating
     const updateButton = document.createElement('button');
 
     updateButton.textContent = 'Update';
     updateButton.addEventListener('click', () => {
-      updateButton.classList.add('hidden'); // Initially hide the Update button
-      // Turn the name and price columns into input areas
+      updateButton.classList.add('hidden'); 
       cellName.innerHTML = `<input type="text" value="${item.name}" />`;
       cellPrice.innerHTML = `<input type="text" value="${item.price.replace(/\$/g, '')}" />`;
 
-      // Create a "Finish" button for finalizing the update
       const finishButton = document.createElement('button');
       finishButton.textContent = 'Finish';
       finishButton.addEventListener('click', () => {
         const newName = cellName.querySelector('input').value;
         const newPrice = cellPrice.querySelector('input').value;
 
-        // Validate inputs before sending the PUT request
         if (newName && !isNaN(newPrice)) {
           updateItem(item.id, { name: newName, price: '$'+newPrice });
         } else {
@@ -67,11 +56,9 @@ function displayData(data) {
         }
       });
 
-      // Append the "Finish" button to the cell
       cellModify.appendChild(finishButton);
     });
 
-    // Append the buttons to the cell
     cellModify.appendChild(updateButton);
     cellModify.appendChild(deleteButton);
   });
@@ -80,18 +67,15 @@ function displayData(data) {
 
 function deleteItem(itemId) {
   const token = localStorage.getItem('token');
-  // Replace 'YOUR_SERVER_URL' with your actual server URL
   fetch(`http://localhost:8080/api/v1/lighter/${itemId}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
-      // Add any additional headers if required
     },
   })
   .then(response => {
     if (response.ok) {
-      // Item deleted successfully, fetch updated data and refresh the table
       fetchData();
       console.log(`Item with ID ${itemId} deleted successfully`);
     } else {
@@ -103,7 +87,6 @@ function deleteItem(itemId) {
 
 function updateItem(itemId, updatedProduct) {
   const token = localStorage.getItem('token');
-    // Replace 'YOUR_SERVER_URL' with your actual server URL
     fetch(`http://localhost:8080/api/v1/lighter/${itemId}`, {
         method: 'PUT',
         headers: {
@@ -114,7 +97,6 @@ function updateItem(itemId, updatedProduct) {
   })
   .then(response => {
     if (response.ok) {
-      // Item created successfully, fetch updated data and refresh the table
       fetchData();
       console.log('Item successfully updated');
     } else {
@@ -133,21 +115,18 @@ function addNewRow() {
   const cellPrice = newRow.insertCell(1);
   const cellCreate = newRow.insertCell(2);
 
-  // Create input fields for 'name' and 'price'
   const inputName = document.createElement('input');
   const inputPrice = document.createElement('input');
 
   cellName.appendChild(inputName);
   cellPrice.appendChild(inputPrice);
 
-  // Create a button for creating a new row
   const createButton = document.createElement('button');
   createButton.textContent = 'Create';
   createButton.addEventListener('click', () => {
     const newName = inputName.value;
     const newPrice = inputPrice.value;
 
-    // Validate inputs before sending the POST request
     if (newName && !isNaN(newPrice)) {
       createNewItem({ name: newName, price: '$'+newPrice });
     } else {
@@ -156,13 +135,11 @@ function addNewRow() {
     createNewRow.style.display = 'block';
   });
 
-  // Append the button to the cell
   cellCreate.appendChild(createButton);
 }
 
 function createNewItem(newItem) {
   const token = localStorage.getItem('token');
-  // Replace 'YOUR_SERVER_URL' with your actual server URL
   fetch('http://localhost:8080/api/v1/lighter', {
     method: 'POST',
     headers: {
@@ -173,7 +150,6 @@ function createNewItem(newItem) {
   })
   .then(response => {
     if (response.ok) {
-      // Item created successfully, fetch updated data and refresh the table
       fetchData();
       console.log('Item created successfully');
     } else {
@@ -184,7 +160,6 @@ function createNewItem(newItem) {
 }
 
 function logout() {
-  // Assuming you want to redirect to a login page named 'login.html'
   window.location.href = 'http://127.0.0.1:5500/Front-end2/login.html';
 }
 
